e117eead19b42037b7348d7a9636e63c8c0857aa
dilsilva <dilsilva.diego@gmail.com>
2019-02-05 19:49:59 -0200

    Update setup-yum.sh

--- setup-yum.sh
+++ setup-yum.sh
@@ -61,3 +61,29 @@ sudo yum install -y java-1.8.0-openjdk
 
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.181-7.b13.fc28.x86_64/jre
 export PATH=$JAVA_HOME/bin:$PATH
+
+##Kubernetes
+#Kubectl
+
+sudo yum install -y kubectl 
+
+#Helm 
+
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get > get_helm.sh
+chmod 700 get_helm.sh
+./get_helm.sh
+
+helm version
+
+rm -rf get_helm.sh
+
+
+#IAC Tools
+#Terraform
+
+wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
+unzip terraform_0.11.11_linux_amd64.zip /terraform
+cd terraform
+sudo mv terraform /usr/local/bin/
+
+sudo terraform --version 
